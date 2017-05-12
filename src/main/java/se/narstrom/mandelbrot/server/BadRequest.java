bf10b8a3f9d7a11c3ee3e9dee31c1a3e8a4a68cb
Rickard Närström <rickard@narstrom.se>
2017-05-12 04:40:51 +0200

    Remove some magic numbers (HTTP status codes).

--- src/main/java/se/narstrom/mandelbrot/server/BadRequest.java
+++ src/main/java/se/narstrom/mandelbrot/server/BadRequest.java
@@ -1,13 +1,11 @@
 package se.narstrom.mandelbrot.server;
 
+import javax.servlet.http.HttpServletResponse;
+
 public class BadRequest extends HttpError {
 	private static final long serialVersionUID = 1L;
 
-	public BadRequest() {
-		super(400, "Bad Request");
-	}
-
 	public BadRequest(Throwable cause) {
-		super(400, cause);
+		super(HttpServletResponse.SC_BAD_REQUEST, cause);
 	}
 }
--- src/main/java/se/narstrom/mandelbrot/server/NotFound.java
+++ src/main/java/se/narstrom/mandelbrot/server/NotFound.java
@@ -1,9 +1,11 @@
 package se.narstrom.mandelbrot.server;
 
+import javax.servlet.http.HttpServletResponse;
+
 public class NotFound extends HttpError {
 	private static final long serialVersionUID = 1L;
 
 	public NotFound() {
-		super(404, "Not Found");
+		super(HttpServletResponse.SC_NOT_FOUND, "Not Found");
 	}
 }
